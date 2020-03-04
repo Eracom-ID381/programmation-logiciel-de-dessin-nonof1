dfb51b10f9f3a8dd83dadab7a16f69b3aabd4c4b
nonof1 <60931115+nonof1@users.noreply.github.com>
2020-03-04 14:21:33 +0100

    04.03.20 / Started project
    - Changed background color
    - MouseIsPressed to draw
    - Left and right arrows to change brush size

--- js/sketch.js
+++ js/sketch.js
@@ -1,15 +1,34 @@
+let brushSize = 10;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(0);
-  noStroke();
+    createCanvas(windowWidth, windowHeight);
+    background(102);
+    noStroke();
 }
 
 function draw() {
-  fill(255);
-  rect(mouseX, mouseY, 50, 50);
+    fill(255);
+    stroke(255);
+    strokeWeight(brushSize);
+
+    if (mouseIsPressed === true) {
+        line(mouseX, mouseY, pmouseX, pmouseY);
+    }
+}
+
+function keyPressed() {
+    if (keyCode === LEFT_ARROW) {
+        brushSize = brushSize - 5;
+    }
+    if (keyCode === RIGHT_ARROW) {
+        brushSize = brushSize + 5;
+    }
+    if (keyCode === "G") {
+        fill(0);
+    }
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  background(0);
+    resizeCanvas(windowWidth, windowHeight);
+    background(102);
 }
