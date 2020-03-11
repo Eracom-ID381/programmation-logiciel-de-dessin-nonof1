6151a2aac2ecfa21c98488eb13ea75dd569c1eb7
nonof1 <60931115+nonof1@users.noreply.github.com>
2020-03-11 15:08:19 +0100

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,49 +1,50 @@
 let cursor;
 let graphics;
 let brushSize = 10;
-let hue = 2;
+let hue = 0;
 let tint = 255;
 let lum = 255;
 
 function preload() {
-  cursor = loadImage("../img/paintbrush.png");
+    cursor = loadImage("../img/paintbrush.png");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  pixelDensity(1);
-  graphics = createGraphics(windowWidth, windowHeight);
-  graphics.background(255);
+    createCanvas(windowWidth, windowHeight);
+    pixelDensity(1);
+    graphics = createGraphics(windowWidth, windowHeight);
+    graphics.background(255);
 }
 
 function draw() {
-  background(255);
-  image(graphics, 0, 0);
+    background(255);
 
-  if (mouseIsPressed) {
-    graphics.stroke(0);
-    graphics.strokeWeight(10);
-    graphics.line(mouseX, mouseY, pmouseX, pmouseY);
-  }
-  image(cursor, mouseX, mouseY);
+    image(graphics, 0, 0);
+
+    if (mouseIsPressed) {
+        graphics.stroke(hue);
+        graphics.strokeWeight(brushSize);
+        graphics.line(mouseX, mouseY, pmouseX, pmouseY);
+    }
+    image(cursor, mouseX, mouseY, 30, 30);
 }
 
 function keyPressed() {
-  if (keyCode === DOWN_ARROW) {
-    brushSize = brushSize - 5;
-  }
-  if (keyCode === UP_ARROW) {
-    brushSize = brushSize + 5;
-  }
-  if (keyCode === RIGHT_ARROW) {
-    hue = hue - 10;
-  }
-  if (keyCode === LEFT_ARROW) {
-    hue = hue + 10;
-  }
+    if (keyCode === DOWN_ARROW) {
+        brushSize = brushSize - 5;
+    }
+    if (keyCode === UP_ARROW) {
+        brushSize = brushSize + 5;
+    }
+    if (keyCode === RIGHT_ARROW) {
+        hue = hue - 10;
+    }
+    if (keyCode === LEFT_ARROW) {
+        hue = hue + 10;
+    }
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  background(255);
+    resizeCanvas(windowWidth, windowHeight);
+    background(255);
 }
