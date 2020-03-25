baf0671205ae43defe0f2a61db30bcf3fa96c598
nonof1 <60931115+nonof1@users.noreply.github.com>
2020-03-25 15:42:07 +0100

    Finished palette

--- js/sketch.js
+++ js/sketch.js
@@ -11,6 +11,12 @@ let lum = 255;
 let palette;
 let white;
 let red;
+let orange;
+let yellow;
+let green;
+let bleuClair;
+let bleuFonce;
+let violet;
 
 function preload() {
     cursor = loadImage("../img/paintbrush.png");
@@ -45,63 +51,6 @@ function draw() {
     drawSidebar();
 }
 
-function keyPressed() {
-    if (keyCode === DOWN_ARROW) {
-        brushSize = brushSize - 5;
-    }
-    if (keyCode === UP_ARROW) {
-        brushSize = brushSize + 5;
-    }
-    if (key === '1') {
-        hue = 01;
-        sat = 255;
-        lum = 255;
-    }
-    if (key === '2') {
-        hue = 20;
-        sat = 255;
-        lum = 255;
-    }
-    if (key === '3') {
-        hue = 35;
-        sat = 255;
-        lum = 255;
-    }
-    if (key === '4') {
-        hue = 60;
-        sat = 255;
-        lum = 255;
-    }
-    if (key === '5') {
-        hue = 130;
-        sat = 255;
-        lum = 255;
-    }
-    if (key === '6') {
-        hue = 160;
-        sat = 255;
-        lum = 255;
-    }
-    if (key === '7') {
-        hue = 190;
-        sat = 255;
-        lum = 255;
-    }
-    if (key === '8') {
-        hue = 0;
-        sat = 0;
-        lum = 0;
-    }
-    if (key === '9') {
-        hue = 0;
-        sat = 0;
-        lum = 255;
-        cursor = loadImage("../img/eraser.png");
-    } else {
-        cursor = loadImage("../img/paintbrush.png");
-    }
-}
-
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     background(255);
@@ -130,87 +79,122 @@ function drawSidebar() {
 
 function drawColorpalette() {
 
-    //Rouge
-    strokeWeight(2);
-    fill(0, 255, 255);
-    rect(40, 190, 35, 35);
-    strokeWeight(0);
-    fill(0);
-    text("1", 53, 214);
-
-    //Orange
-    strokeWeight(2);
-    fill(20, 255, 255);
-    rect(110, 190, 35, 35);
-    strokeWeight(0);
-    fill(0);
-    text("2", 122, 214);
-
-    //Jaune
-    strokeWeight(2);
-    fill(35, 255, 255);
-    rect(40, 250, 35, 35);
-    strokeWeight(0);
-    fill(0);
-    text("3", 53, 274);
-
-    //Vert
-    strokeWeight(2);
-    fill(60, 255, 255);
-    rect(110, 250, 35, 35);
-    strokeWeight(0);
-    fill(0);
-    text("4", 122, 274);
-
-    //Bleu clair
-    strokeWeight(2);
-    fill(130, 255, 255);
-    rect(40, 310, 35, 35);
-    strokeWeight(0);
-    fill(0);
-    text("5", 52, 333);
-
-    //Bleu foncé
-    strokeWeight(2);
-    fill(160, 255, 255);
-    rect(110, 310, 35, 35);
-    strokeWeight(0);
-    fill(0);
-    text("6", 122, 333);
-
-    //Violet
-    strokeWeight(2);
-    fill(190, 255, 255);
-    rect(40, 370, 35, 35);
-    strokeWeight(0);
-    fill(0);
-    text("7", 52, 394);
-
     //Black
     palette = createButton("Noir");
     palette.mousePressed(noir);
     palette.style("background-color", "black");
-    palette.position(40, 465);
+    palette.position(40, 300);
     palette.size(40, 40);
 
     //white
     white = createButton("");
     white.mousePressed(blanc);
-    white.position(90, 465);
+    white.position(90, 300);
     white.size(40, 40);
 
     //red
     red = createButton("");
-    red.mousePressed(red);
+    red.mousePressed(rouge);
     red.style("background-color", "red");
     red.position(40, 180);
     red.size(40, 40);
+
+    //orange
+    orange = createButton("");
+    orange.mousePressed(orang);
+    orange.style("background-color", "#ff6600");
+    orange.position(90, 180);
+    orange.size(40, 40);
+
+    //yellow
+    yellow = createButton("");
+    yellow.mousePressed(jaune);
+    yellow.style("background-color", "#ffcc00");
+    yellow.position(140, 180);
+    yellow.size(40, 40);
+
+    //green
+    green = createButton("");
+    green.mousePressed(vert);
+    green.style("background-color", "#00ff00");
+    green.position(40, 230);
+    green.size(40, 40);
+
+    //light blue
+    bleuClair = createButton("");
+    bleuClair.mousePressed(bleu01);
+    bleuClair.style("background-color", "#00ffff");
+    bleuClair.position(90, 230);
+    bleuClair.size(40, 40);
+
+    //dark blue
+    bleuFonce = createButton("");
+    bleuFonce.mousePressed(bleu02);
+    bleuFonce.style("background-color", "#0000ff");
+    bleuFonce.position(90, 230);
+    bleuFonce.size(40, 40);
+
+    //purple
+    violet = createButton("");
+    violet.mousePressed(violett);
+    violet.style("background-color", "#6600ff");
+    violet.position(140, 230);
+    violet.size(40, 40);
 }
 
 function noir() {
     hue = 0;
     sat = 0;
     lum = 0;
+    cursor = loadImage("../img/paintbrush.png");
+}
+
+function rouge() {
+    hue = 01;
+    sat = 255;
+    lum = 255;
+    cursor = loadImage("../img/paintbrush.png");
+}
+
+function orang() {
+    hue = 20;
+    sat = 255;
+    lum = 255;
+    cursor = loadImage("../img/paintbrush.png");
+}
+
+function jaune() {
+    hue = 35;
+    sat = 255;
+    lum = 255;
+    cursor = loadImage("../img/paintbrush.png");
+}
+
+function vert() {
+    hue = 60;
+    sat = 255;
+    lum = 255;
+    cursor = loadImage("../img/paintbrush.png");
+}
+
+function bleu01() {
+    hue = 130;
+    sat = 255;
+    lum = 255;
+    cursor = loadImage("../img/paintbrush.png");
+}
+
+function bleu02() {
+    hue = 160;
+    sat = 255;
+    lum = 255;
+}
+
+function violett() {
+    hue = 190;
+    sat = 255;
+    lum = 255;
+    cursor = loadImage("../img/paintbrush.png");
 }
 
 function blanc() {
